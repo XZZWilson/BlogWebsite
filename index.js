c10f6e03994603816845a21e1d051353d5685a08
Wilson <wislonni0@gmail.com>
2025-12-30 01:58:48 -0500

    Post Creation feature

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const posts = [];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { posts });
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/create", (req, res) => {
+    const newPost = {
+        id: Date.now(),
+        subject: req.body.subject.trim(),
+        content: req.body.content.trim(),
+        createdOn: new Date() 
+    };
+    posts.push(newPost);
+    res.redirect("/post");
 });
 
 app.listen(port, () => {
